0e2e22659807d74a22bbf55b6aef5a51328fae34
phvinh1412 <vinhphamhub@gmail.com>
2020-10-18 13:25:01 +0700

    Update for scraping

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module vinh.crawler
 go 1.15
 
 require (
-	github.com/steelx/extractlinks v0.0.2 // indirect
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/steelx/extractlinks v0.0.2
 	golang.org/x/net v0.0.0-20201010224723-4f7140c49acb // indirect
 )
--- ultilities/ultilities.go
+++ ultilities/ultilities.go
@@ -1,12 +1,45 @@
 package ultilities
 
-import "fmt"
+import (
+	"fmt"
+	"crypto/tls"
+	"net/http"
+	"os"
+	"github.com/steelx/extractlinks"
+)
 
 func init() {  
     fmt.Println("Ultilities package initialized")
 }
 
-func GetLink() {
-	fmt.Print("VINH____")
+func ExtractLinks(usingUrl string){
+
+	tls_config := &tls.Config{
+		InsecureSkipVerify: true,
+	}
+	transport := &http.Transport{
+		TLSClientConfig: tls_config,
+	}
+	client := &http.Client{
+		Transport: transport,
+	}
+
+	response, err := client.Get(usingUrl)
+
+	CheckError(err)
+	defer response.Body.Close()
+
+	links, err := extractlinks.All(response.Body)
+	CheckError(err)
+	for i, link := range links{
+		fmt.Printf("Index: %v\tLink: %v\n",i, link)
+	}
+}
+
+func CheckError(err error) {
+	if err != nil {
+		fmt.Print("Error: ", err)
+		os.Exit(1)
+	}
 }
 
